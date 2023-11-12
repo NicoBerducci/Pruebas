9d90985d64d5ad93ed4c9d021b15638c66a5e9b0
MartinGenaulaz05 <lgenaulaz@gmail.com>
2023-11-11 22:50:50 -0300

    seguridad

---
+++ LosEruditos-Backend-main/src/main/java/com/tup/buensabor/Authentication/AuthService.java
@@ -0,0 +1,48 @@
+package com.tup.buensabor.Authentication;
+
+import com.tup.buensabor.entities.Usuario;
+import com.tup.buensabor.enums.Rol;
+import com.tup.buensabor.repositories.UsuarioRepository;
+import com.tup.buensabor.Jwt.JwtService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthService {
+
+    private final UsuarioRepository usuarioRepository;
+    private final JwtService jwtService;
+    private final PasswordEncoder passwordEncoder;
+    private final AuthenticationManager authenticationManager;
+
+    public AuthResponse login(LoginRequest request) {
+        authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(request.getUsername(), request.getPassword()));
+        UserDetails usuario= (UserDetails) usuarioRepository.findByUsername(request.getUsername()).orElseThrow();
+        String token=jwtService.getToken(usuario);
+        return AuthResponse.builder()
+                .token(token)
+                .build();
+
+    }
+
+    public AuthResponse register(RegisterRequest request) {
+        Usuario usuario = Usuario.builder()
+                .username(request.getUsername())
+                .password(passwordEncoder.encode( request.getPassword()))
+
+                .build();
+
+        usuarioRepository.save(usuario);
+
+        return AuthResponse.builder()
+                .token(jwtService.getToken((UserDetails) usuario))
+                .build();
+
+    }
+
+}
